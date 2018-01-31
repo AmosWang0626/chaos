c5a77b455801e43ccc8cc67365a5915e05df46ce
daoyuan <1833063210@qq.com>
2018-01-31 11:50:14 +0800

    优化日期

--- chaos-simple/src/main/java/cn/amos/simple/base/DateCompare.java
+++ chaos-simple/src/main/java/cn/amos/simple/base/DateCompare.java
@@ -1,50 +1,67 @@
 package cn.amos.simple.base;
 
+import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 
+/**
+ * @author DaoyuanWang
+ */
 public class DateCompare {
 
     private static Calendar calendar = Calendar.getInstance();
-
-    private static SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-
-//    private static SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+    /**
+     * 日期格式化
+     */
+    private static final ThreadLocal<DateFormat> FORMAT_YEAR_2_SECOND =
+            ThreadLocal.withInitial(() -> new SimpleDateFormat("yyyy-MM-dd HH:mm:ss"));
+    private static final ThreadLocal<DateFormat> FORMAT_YEAR_2_DAY =
+            ThreadLocal.withInitial(() -> new SimpleDateFormat("yyyy-MM-dd"));
+    private static final ThreadLocal<DateFormat> FORMAT_YEAR_2_MONTH =
+            ThreadLocal.withInitial(() -> new SimpleDateFormat("yyyy-MM"));
 
     public static void main(String[] args) {
-//        Date testDate = null;
-//        try {
-//            testDate = sdf.parse("2017-9-17");
-//        } catch (ParseException e) {
-//            e.printStackTrace();
-//        }
-//        Date testDay = getTestDay(testDate, -6);
-//        System.out.println(testDate.getTime());
-//        System.out.println(sdf.format(testDate));
-//        System.out.println(sdf.format(testDay));
+        formatString2Date();
+    }
 
+    /**
+     * 将字符串转化为日期
+     */
+    private static void formatString2Date() {
+        try {
+            Date testDate = FORMAT_YEAR_2_MONTH.get().parse("2017-09");
+            System.out.println(FORMAT_YEAR_2_DAY.get().format(testDate));
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * calendar 转化日期
+     */
+    private void calendarFormat() {
         calendar.add(Calendar.HOUR_OF_DAY, -2);
         Date time = calendar.getTime();
-        System.out.println(sdf.format(time));
+        System.out.println(FORMAT_YEAR_2_SECOND.get().format(time));
         System.out.println(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(time));
-
-
 
         // 超过五分钟的认证中,重新执行任务
         calendar = Calendar.getInstance();
         calendar.add(Calendar.MINUTE, -5);
-//        long timeInMillis = calendar.getTimeInMillis();
-//        long currentTimeMillis = System.currentTimeMillis();
         time = calendar.getTime();
-        System.out.println(sdf.format(time));
+        System.out.println(FORMAT_YEAR_2_SECOND.get().format(time));
 
-//        System.out.println("SSS:" + (currentTimeMillis - timeInMillis) / (1000 * 60));
-
+        // long timeInMillis = calendar.getTimeInMillis();
+        // long currentTimeMillis = System.currentTimeMillis();
+        // System.out.println("SSS:" + (currentTimeMillis - timeInMillis) / (1000 * 60));
     }
 
-    private static void getYMD() {
+    /**
+     * 根据日期获取当前的年月日
+     */
+    private static void getYearMonthDay() {
         calendar.setTime(new Date());
         String year = calendar.get(Calendar.YEAR) + "";
         int month = calendar.get(Calendar.MONTH) + 1;
@@ -52,12 +69,25 @@ public class DateCompare {
         System.out.println(year + "年" + month + "月" + day + "日");
     }
 
+    /**
+     * 获取指定日期当前月第一天
+     *
+     * @param date 日期
+     * @return 日期
+     */
     private static Date getPreDay(Date date) {
         calendar.setTime(date);
         calendar.add(Calendar.DAY_OF_MONTH, -1);
         return calendar.getTime();
     }
 
+    /**
+     * 获取指定日期
+     *
+     * @param date 当前日期
+     * @param i    参数
+     * @return 日期
+     */
     private static Date getTestDay(Date date, int i) {
         calendar.setTime(date);
         calendar.add(Calendar.DAY_OF_MONTH, i);
