9e9d784c7084b5ce5b757b73dd3e718bdd492513
daoyuan <dywang@firstqianbao.com>
2018-07-09 19:21:33 +0800

    优化冒泡排序算法

--- chaos-algorithm/src/main/java/cn/amos/algorithm/sort/MaoPao.java
+++ chaos-algorithm/src/main/java/cn/amos/algorithm/sort/MaoPao.java
@@ -2,45 +2,66 @@ package cn.amos.algorithm.sort;
 
 import java.util.Random;
 
+/**
+ * @author DaoyuanWang
+ */
 public class MaoPao {
 
     public static void main(String[] args) {
+        // 产生 n 个随机数
         int[] array = new int[15];
-        boolean boo;
-
-        // 随机产生数据并输出
-        System.out.print("0原始：");
         for (int i = 0; i < array.length; i++) {
-            array[i] = new Random().nextInt(30);
-            System.out.print(array[i] + "\t");
+            array[i] = new Random().nextInt(70);
         }
+        printArr(array, 0);
+
+        // 如果已有序排列,则根据count结束for循环
+        int count;
 
         // 冒泡排序
         for (int i = 0; i < array.length - 1; i++) {
-
-            boo = false;
-
+            count = 0;
             for (int j = 0; j < array.length - i - 1; j++) {
                 if (array[j] > array[j + 1]) {
                     swap(array, j);
-                    boo = true;
+                } else {
+                    count++;
                 }
             }
-            // 每次排序后遍历输出
-            System.out.print("\n第" + (i + 1) + "次，");
-            for (int num : array) {
-                System.out.print(num + "\t");
-            }
-            if (!boo) {
+
+            if (count == array.length - i - 1) {
                 return;
             }
+
+            // 每次排序后遍历输出
+            printArr(array, i + 1);
         }
     }
 
-    private static void swap(int[] array, int j) {
-        array[j] = array[j] + array[j + 1];
-        array[j + 1] = array[j] - array[j + 1];
-        array[j] = array[j] - array[j + 1];
+    /**
+     * arr[j] 与 arr[j+1] 内容交换
+     * <code>
+     * int[] arr = new int[]{89, 10};
+     * System.out.println("num1: " + arr[0] + ", num2: " + arr[1]);
+     * swap(arr, 0);
+     * System.out.println("num1: " + arr[0] + ", num2: " + arr[1]);
+     * </code>
+     *
+     * @param array 数组
+     * @param index index
+     */
+    private static void swap(int[] array, int index) {
+        array[index] = array[index] + array[index + 1];
+        array[index + 1] = array[index] - array[index + 1];
+        array[index] = array[index] - array[index + 1];
+    }
+
+    private static void printArr(int[] arr, int num) {
+        System.out.print("第\t" + num + "\t次排序:\t");
+        for (int i : arr) {
+            System.out.print(i + "\t");
+        }
+        System.out.println();
     }
 
 }
