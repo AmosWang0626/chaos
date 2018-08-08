7f94d1adc7636a56e25fe52a1105a27d1d66323a
DAOYUAN <1833063210@qq.com>
2018-08-08 23:14:13 +0800

    完善单链表demo

--- chaos-algorithm/src/main/java/cn/amos/algorithm/node/Main.java
+++ chaos-algorithm/src/main/java/cn/amos/algorithm/node/Main.java
@@ -10,9 +10,30 @@ package cn.amos.algorithm.node;
 public class Main {
 
     public static void main(String[] args) {
-        Node<String, String> node = new Node<>("initKey", "initValue", null);
+        Node<String, String> node = new Node<>("initKey", "initValue");
+        Node<String, String> temp = node;
         for (int i = 0; i < 10; i++) {
-
+            temp = temp.setNext(new Node<>("key " + i, "val " + i));
         }
+        System.out.println(node.toString());
+    }
+}
+
+class Node<K, V> {
+
+    final K key;
+    
+    V value;
+
+    Node<K, V> next;
+
+    Node(K key, V value) {
+        this.key = key;
+        this.value = value;
+    }
+
+    Node<K, V> setNext(Node<K, V> next) {
+        this.next = next;
+        return next;
     }
 }
