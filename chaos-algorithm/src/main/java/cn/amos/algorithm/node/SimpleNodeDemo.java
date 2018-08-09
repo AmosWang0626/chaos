9fd9bc93e8be2dc5ad688d9ee272fa9df41435fa
dywang <dywang@1-cash.cn>
2018-08-09 10:06:02 +0800

    完善node删除、添加

---
+++ chaos-algorithm/src/main/java/cn/amos/algorithm/node/SimpleNodeDemo.java
@@ -0,0 +1,60 @@
+package cn.amos.algorithm.node;
+
+/**
+ * NOTE: 类说明
+ * PROJECT: chaos
+ *
+ * @author AMOS
+ * @date 2018/8/8
+ */
+public class SimpleNodeDemo {
+
+    public static void main(String[] args) {
+        Node<String> node = new Node<>("head");
+        Node<String> temp = node;
+        for (int i = 0; i < 10; i++) {
+            temp = temp.add(new Node<>("data " + i));
+        }
+
+        int i = 0;
+        while (node.next != null) {
+            // 添加节点
+            if (++i == 3) {
+                Node<String> insert = new Node<>("insert " + i);
+                insert.next = node.next;
+                node.next = insert;
+            }
+            // 删除节点
+            if (i == 5) {
+                node.next = node.next.next;
+            }
+            System.out.println(node.next);
+            node = node.next;
+        }
+    }
+}
+
+class Node<D> {
+
+    private final D data;
+
+    Node<D> next;
+
+    Node(D data) {
+        this.data = data;
+    }
+
+    public D getData() {
+        return data;
+    }
+
+    Node<D> add(Node<D> next) {
+        this.next = next;
+        return next;
+    }
+
+    @Override
+    public String toString() {
+        return "Node{" + "data=" + data + '}';
+    }
+}
