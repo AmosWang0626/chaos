ce4e5ec233089f0156bd85b5a8dc42806ed388b2
amos.wang <1833063210@qq.com>
2020-05-11 08:04:16 +0800

    feat: AtomicStampedReference改进CAS

--- chaos-advanced/src/main/java/com/amos/advanced/thread/cas/CasAtomicInteger2.java
+++ chaos-advanced/src/main/java/com/amos/advanced/thread/cas/CasAtomicInteger2.java
@@ -2,6 +2,7 @@ package com.amos.advanced.thread.cas;
 
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicStampedReference;
 
 /**
  * DESCRIPTION: 简单测试 AtomicInteger 实现 CAS
@@ -12,29 +13,58 @@ import java.util.concurrent.atomic.AtomicInteger;
 public class CasAtomicInteger2 {
 
     private static AtomicInteger ai = new AtomicInteger(100);
+    private static AtomicStampedReference<Integer> asr
+            = new AtomicStampedReference<>(100, 1);
 
     public static void main(String[] args) throws InterruptedException {
         aiCompareAndSet();
+        System.out.println(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
+        asrCompareAndSet();
+    }
+
+    public static void asrCompareAndSet() throws InterruptedException {
+        new Thread(() -> {
+            boolean compareAndSet = asr.compareAndSet(100, 120,
+                    asr.getStamp(), asr.getStamp() + 1);
+            System.out.println("asrCompareAndSet(100, 120) >>>>> " + compareAndSet);
+            boolean compareAndSet2 = asr.compareAndSet(120, 100,
+                    asr.getStamp(), asr.getStamp() + 1);
+            System.out.println("asrCompareAndSet(120, 100) >>>>> " + compareAndSet2);
+        }).start();
+
+        new Thread(() -> {
+            int stamp = asr.getStamp();
+            try {
+                TimeUnit.SECONDS.sleep(1);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            boolean compareAndSet = asr.compareAndSet(120, 100,
+                    stamp, stamp + 1);
+            System.out.println("asrCompareAndSet(120, 100) >>>>> " + compareAndSet);
+        }).start();
+
+        System.out.println("AtomicStampedReference<Integer> CompareAndSet Finish >>>>> " + ai.get());
     }
 
     public static void aiCompareAndSet() throws InterruptedException {
         new Thread(() -> {
             boolean compareAndSet = ai.compareAndSet(100, 120);
-            System.out.println("compareAndSet(100, 120) >>>>> " + compareAndSet);
+            System.out.println("aiCompareAndSet(100, 120) >>>>> " + compareAndSet);
         }).start();
 
         TimeUnit.SECONDS.sleep(1);
 
         new Thread(() -> {
             boolean compareAndSet = ai.compareAndSet(120, 100);
-            System.out.println("compareAndSet(120, 100) >>>>> " + compareAndSet);
+            System.out.println("aiCompareAndSet(120, 100) >>>>> " + compareAndSet);
         }).start();
 
         TimeUnit.SECONDS.sleep(1);
 
         new Thread(() -> {
             boolean compareAndSet = ai.compareAndSet(100, 188);
-            System.out.println("compareAndSet(100, 188) >>>>> " + compareAndSet);
+            System.out.println("aiCompareAndSet(100, 188) >>>>> " + compareAndSet);
         }).start();
 
         System.out.println("AtomicInteger CompareAndSet Finish >>>>> " + ai.get());
