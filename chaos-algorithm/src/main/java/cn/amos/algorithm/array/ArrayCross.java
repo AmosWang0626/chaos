2c764c4fddc1999cd6b62e5ae9d9d2b6b2c53659
amos.wang <1833063210@qq.com>
2020-06-22 23:46:57 +0800

    feat: 算法第一天（两数组求交集，暴力解法）

--- chaos-algorithm/src/main/java/cn/amos/algorithm/array/ArrayCross.java
+++ chaos-algorithm/src/main/java/cn/amos/algorithm/array/ArrayCross.java
@@ -12,30 +12,90 @@ import java.util.Arrays;
 public class ArrayCross {
 
     public static void main(String[] args) {
-        int[] nums1 = new int[]{1, 4, 6, 7, 2, 8};
-        int[] nums2 = new int[]{2, 5, 7, 4, 8, 9};
+        /// random array
+//        final int arr1 = 20;
+//        int[] nums1 = new int[arr1];
+//        for (int i = 0; i < arr1; i++) {
+//            nums1[i] = new Random().nextInt(50);
+//        }
+//        final int arr2 = 30;
+//        int[] nums2 = new int[arr2];
+//        for (int i = 0; i < arr2; i++) {
+//            nums2[i] = new Random().nextInt(50);
+//        }
+//        System.out.println(Arrays.toString(nums1));
+//        System.out.println(Arrays.toString(nums2));
+
+        int[] nums1 = {61, 24, 20, 58, 95, 53, 17, 32, 45, 85, 70, 20, 83, 62, 35, 89, 5, 95, 12, 86, 58, 77, 30, 64, 46, 13, 5, 92, 67, 40, 20, 38, 31, 18, 89, 85, 7, 30, 67, 34, 62, 35, 47, 98, 3, 41, 53, 26, 66, 40, 54, 44, 57, 46, 70, 60, 4, 63, 82, 42, 65, 59, 17, 98, 29, 72, 1, 96, 82, 66, 98, 6, 92, 31, 43, 81, 88, 60, 10, 55, 66, 82, 0, 79, 11, 81};
+        int[] nums2 = {5, 25, 4, 39, 57, 49, 93, 79, 7, 8, 49, 89, 2, 7, 73, 88, 45, 15, 34, 92, 84, 38, 85, 34, 16, 6, 99, 0, 2, 36, 68, 52, 73, 50, 77, 44, 61, 48};
+
         System.out.println(Arrays.toString(intersect(nums1, nums2)));
+//        System.out.println(Arrays.toString(removeByIndex(nums2, 1)));
     }
 
     public static int[] intersect(int[] nums1, int[] nums2) {
-//        int nums1length = nums1.length;
-//        int nums2length = nums2.length;
-//        // index && mergeNums
-//        int index = 0;
-//        int[] mergeNums = new int[Math.min(nums1length, nums2length)];
-//
-//        int surplusIndex = 0;
-//        if (nums1length < nums2length) {
-//            for (int i : nums1) {
-//                for (int j = 0; j < nums2length - surplusIndex; j++) {
-//                    if (i == nums2[j]) {
-//                        surplusIndex++;
-//                    }
-//                }
-//            }
-//        }
+        // 空数组
+        int[] nil = {};
 
-        return null;
+        if (nums1.length == 0 || nums2.length == 0) {
+            return nil;
+        }
+
+        // leftArr 为数目较多的数组
+        int[] leftArr;
+        int[] rightArr;
+
+        if (nums1.length > nums2.length) {
+            leftArr = nums1;
+            rightArr = nums2;
+        } else {
+            leftArr = nums2;
+            rightArr = nums1;
+        }
+
+        // 数组指针
+        int index = 0;
+        // 初始化数组, 重复的个数最大为 rightArr.length
+        int[] crossArr = new int[rightArr.length];
+
+        for (int left : leftArr) {
+            for (int i = 0; i < rightArr.length; i++) {
+                if (left == rightArr[i]) {
+                    crossArr[index] = left;
+                    index++;
+                    rightArr = removeByIndex(rightArr, i);
+                    
+                    // 至关重要!!!
+                    break;
+                }
+            }
+        }
+
+        if (index == 0) {
+            return nil;
+        }
+
+        int[] tempArr = new int[index];
+        if (index >= 0) {
+            System.arraycopy(crossArr, 0, tempArr, 0, index);
+        }
+
+        return tempArr;
+    }
+
+    public static int[] removeByIndex(int[] arr, int index) {
+        int tempIndex = 0;
+        int[] temp = new int[arr.length - 1];
+
+        for (int i = 0; i < arr.length; i++) {
+            if (i == index) {
+                continue;
+            }
+            temp[tempIndex] = arr[i];
+            tempIndex++;
+        }
+
+        return temp;
     }
 
 }
